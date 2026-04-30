6b73b18d0fb88ff63b0670a65698c1be6d1bbf87
obadaDeg <animayoloteer@gmail.com>
2026-05-01 02:41:03 +0300

    chore: add sitemap generation and google site verification

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://obadadeg.github.io',
   integrations: [
     tailwind(),
     icon(),
+    sitemap(),
   ],
   markdown: {
     shikiConfig: {
